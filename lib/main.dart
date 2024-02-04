102d16c3f9246d32b71fdf99603cbc6562ff6ebc
Isaias <igerardo0808@gmail.com>
2024-02-03 20:24:31 -0600

    primer vista de login completada

--- lib/main.dart
+++ lib/main.dart
@@ -26,30 +26,63 @@ class MyApp extends StatelessWidget {
         child: Scaffold(
           body: Column(textDirection: TextDirection.ltr, children: [
             Padding(
-                padding: EdgeInsets.fromLTRB(
-                    _deviceWidth * 0.10, _deviceHeigth * 0.20, 15, 0),
-                child: Container(
-                  width: _deviceWidth * 1,
-                  height: _deviceHeigth * 0.60,
-                  child: Column(
-                    children: [
-                      Container(
-                          width: _deviceHeigth * 0.90,
-                          child: _texto('Login', 30, Colors.black)),
-                      Container(
+              padding: EdgeInsets.fromLTRB(
+                  _deviceWidth * 0.10, _deviceHeigth * 0.20, 15, 0),
+              child: Container(
+                width: _deviceWidth * 1,
+                height: _deviceHeigth * 0.75,
+                child: Column(
+                  children: [
+                    Container(
                         width: _deviceHeigth * 0.90,
-                        child: Text(
-                          'Sign up to continue',
-                          style: TextStyle(
-                              color: Color.fromRGBO(146, 145, 142, 1.0),
-                              fontSize: 25,
-                              fontFamily: "ubuntu",
-                              fontWeight: FontWeight.bold),
-                        ),
-                      )
-                    ],
-                  ),
-                )),
+                        child: _texto('Login', 30, Colors.black)),
+                    Container(
+                      width: _deviceHeigth * 0.90,
+                      child: Text(
+                        'Sign up to continue',
+                        style: TextStyle(
+                            color: Color.fromRGBO(146, 145, 142, 1.0),
+                            fontSize: 25,
+                            fontFamily: "ubuntu",
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: _deviceWidth * 0.90,
+                      child: Text(
+                        'Username',
+                        style: TextStyle(
+                            color: Color.fromRGBO(28, 28, 28, 1.0),
+                            fontSize: 12),
+                      ),
+                    ),
+                    Container(
+                      child: _textBox('youremail@tweak.com'),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      width: _deviceWidth * 0.90,
+                      child: Text(
+                        'Password',
+                        style: TextStyle(
+                            color: Color.fromRGBO(28, 28, 28, 1.0),
+                            fontSize: 12),
+                      ),
+                    ),
+                    _textBox("tweak-employee101234"),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    _btnAcceder(),
+                  ],
+                ),
+              ),
+            ),
           ]),
         ),
       ),
@@ -79,5 +112,13 @@ class MyApp extends StatelessWidget {
         ));
   }
 
+  Widget _textBox(String hintText) {
+    return TextField(
+      onTap: () => {},
+      enabled: true,
+      decoration:
+          InputDecoration(border: OutlineInputBorder(), hintText: hintText),
+    );
+  }
   // Sep
 }
